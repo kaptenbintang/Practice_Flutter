9f6154da2adbb5a84f9e33704e1ddd653cca816b
kaptenbintang <kapten.bintang90@gmail.com>
2022-10-20 17:08:53 +0800

    chore: update new practice

---
+++ loginpage_positioned_floatingactionbutton/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:loginpage_positioned_floatingactionbutton/mainpage.dart';
+// Step 1
+import 'package:flutter/services.dart';
+
+void main() {
+  // Step 2
+  WidgetsFlutterBinding.ensureInitialized();
+  // Step 3
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((value) => runApp(MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
