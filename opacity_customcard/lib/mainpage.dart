18d02ddc898cfce17604295e1c37a7a43273597a
kaptenbintang <kapten.bintang90@gmail.com>
2022-10-20 15:27:37 +0800

    chore: update new practice

---
+++ opacity_customcard/lib/mainpage.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Custom Card Example",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color(0xFF8C062f),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Color(0xFFFE5788), Color(0xFFF56D5D)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter)),
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: Card(
+                elevation: 30,
+                child: Stack(
+                  children: [
+                    Opacity(
+                      opacity: 0.7,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            image: DecorationImage(
+                                image: AssetImage("assets/pattern.png"),
+                                fit: BoxFit.cover)),
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.35,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(4),
+                              topRight: Radius.circular(4)),
+                          image: DecorationImage(
+                              image: AssetImage("assets/sunset.jpg"),
+                              fit: BoxFit.cover)),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(
+                          20,
+                          50 + MediaQuery.of(context).size.height * 0.35,
+                          20,
+                          20),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              "Beautiful sunset at paddy field",
+                              style: TextStyle(
+                                  color: Color(0xFFF56D5D), fontSize: 25),
+                              maxLines: 2,
+                              textAlign: TextAlign.center,
+                            ),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(0, 20, 0, 15),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Posted On",
+                                    style: TextStyle(
+                                        color: Colors.grey, fontSize: 12),
+                                  ),
+                                  Text(
+                                    "June 18 , 2019",
+                                    style: TextStyle(
+                                        color: Color(0xFFF56D5D), fontSize: 12),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Spacer(
+                                  flex: 10,
+                                ),
+                                Icon(
+                                  Icons.thumb_up,
+                                  size: 18,
+                                  color: Colors.grey,
+                                ),
+                                //icon
+                                Spacer(
+                                  flex: 1,
+                                ),
+                                Text(
+                                  "99",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                //text
+                                Spacer(
+                                  flex: 5,
+                                ),
+                                Icon(
+                                  Icons.comment,
+                                  size: 18,
+                                  color: Colors.grey,
+                                ),
+                                //icon
+                                Spacer(
+                                  flex: 1,
+                                ),
+                                Text(
+                                  "888",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                //text
+                                Spacer(
+                                  flex: 10,
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
