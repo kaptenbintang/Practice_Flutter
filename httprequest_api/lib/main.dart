b2f3f2394696d2a11bab8a04f265d15b927a6023
kaptenbintang <kapten.bintang90@gmail.com>
2022-10-21 16:43:20 +0800

    chore: method post API

---
+++ httprequest_api/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:httprequest_api/post_result_model.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  dynamic postResult = null;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("API demo")),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text((postResult != null)
+                ? postResult.id +
+                    " | " +
+                    postResult.name +
+                    " | " +
+                    postResult.job +
+                    " | " +
+                    postResult.created
+                : "tidak ada data"),
+            ElevatedButton(
+                onPressed: () {
+                  PostResult.connectToAPI("Badu", "Dokter").then((value) {
+                    postResult = value;
+                    setState(() {});
+                  });
+                },
+                child: Text("Post"))
+          ],
+        )),
+      ),
+    );
+  }
+}
