737fcef67c1403baad979430d0bce92e3faa71e8
kaptenbintang <kapten.bintang90@gmail.com>
2022-11-10 16:35:36 +0800

    chore: add clientType

--- login_uix_firebase/lib/pages/register_page.dart
+++ login_uix_firebase/lib/pages/register_page.dart
@@ -119,6 +119,7 @@ class _RegisterPageState extends State<RegisterPage> {
       'phoneNumber': countryDial + phoneNumber,
       'dateofbirth': dateofbirth,
       'roles': 'user',
+      'clientType': '',
       'clientcode': getLastInitials(_lastNameController.text.toString()) +
           getInitials(_firstNameController.text.toString()) +
           '-' +
