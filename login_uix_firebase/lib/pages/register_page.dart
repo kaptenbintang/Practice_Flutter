0b8cde74ccebb6e70585b4f57f0c9e06102f4795
FaberScar <zoys.toys@gmail.com>
2022-11-11 17:43:08 +0800

    edit user working, sort ct code, fix table view

--- login_uix_firebase/lib/pages/register_page.dart
+++ login_uix_firebase/lib/pages/register_page.dart
@@ -119,7 +119,7 @@ class _RegisterPageState extends State<RegisterPage> {
       'phoneNumber': countryDial + phoneNumber,
       'dateofbirth': dateofbirth,
       'roles': 'user',
-      'clientType': '',
+      'clientType': 'TBD',
       'clientcode': getLastInitials(_lastNameController.text.toString()) +
           getInitials(_firstNameController.text.toString()) +
           '-' +
