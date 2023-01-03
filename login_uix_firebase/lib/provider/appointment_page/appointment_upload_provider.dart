a31aee3adc92dcd8916ab2b1e81251715feb8005
FaberKing <zoys.toys@gmail.com>
2023-01-03 17:13:27 +0800

    add notifier and provider for appointment uploader

---
+++ login_uix_firebase/lib/provider/appointment_page/appointment_upload_provider.dart
@@ -0,0 +1,8 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:login_uix_firebase/model/typedefs/is_loading.dart';
+import 'package:login_uix_firebase/provider/appointment_page/appointment_upload_notifier.dart';
+
+final appointmentUploaderProvider =
+    StateNotifierProvider<AppointmentUploadNotifier, IsLoading>(
+  (ref) => AppointmentUploadNotifier(),
+);
