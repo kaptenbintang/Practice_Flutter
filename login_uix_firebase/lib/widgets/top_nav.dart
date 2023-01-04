03f84c3a6384655d32498db0a5a60ea13f4d7942
Zuffy <hanafirohani.2902@gmail.com>
2023-01-04 13:42:57 +0800

    fix consumer bug

--- login_uix_firebase/lib/widgets/top_nav.dart
+++ login_uix_firebase/lib/widgets/top_nav.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:login_uix_firebase/auth/provider/user_id_provider.dart';
 import 'package:login_uix_firebase/constant/style.dart';
 import 'package:login_uix_firebase/helper/responsive.dart';
+import 'package:login_uix_firebase/user_info/providers/user_info_model_provider.dart';
+import 'package:login_uix_firebase/widgets/animations/small_error_animation_view.dart';
+import 'package:login_uix_firebase/widgets/animations/small_loading_animation_view.dart';
 import 'package:login_uix_firebase/widgets/custom_text.dart';
 
 import '../flutter_flow/flutter_flow_theme.dart';
@@ -36,19 +40,42 @@ AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) {
     backgroundColor: FlutterFlowTheme.of(context).primaryBtnText,
     automaticallyImplyLeading: true,
     title: Consumer(builder: (context, ref, child) {
+      final uid = ref.watch(userIdProvider);
       final userData = ref.watch(
-        userDetailProvider,
+        userInfoModelProvider(uid.toString()),
       );
       final model = userData.value;
-      return Text(
-        'Welcome' + ' ' + (model?['firstName'] ?? ''),
-        textAlign: TextAlign.center,
-        style: FlutterFlowTheme.of(context).title3.override(
-              fontFamily: 'Poppins',
-              color: FlutterFlowTheme.of(context).primaryText,
-              fontSize: _width / (_maxWidth / 20),
-            ),
-      );
+      if (userData.hasValue) {
+        return userData.when(
+          data: (data) {
+            return Text(
+              'Welcome' + ' ' + (data['firstName'] ?? ''),
+              textAlign: TextAlign.center,
+              style: FlutterFlowTheme.of(context).title3.override(
+                    fontFamily: 'Poppins',
+                    color: FlutterFlowTheme.of(context).primaryText,
+                    fontSize: _width / (_maxWidth / 20),
+                  ),
+            );
+          },
+          loading: () {
+            return const SmallLoadingAnimationView();
+          },
+          error: (Object error, StackTrace stackTrace) {
+            return const SmallErrorAnimationView();
+          },
+        );
+      } else {
+        return Text(
+          'Welcome',
+          textAlign: TextAlign.center,
+          style: FlutterFlowTheme.of(context).title3.override(
+                fontFamily: 'Poppins',
+                color: FlutterFlowTheme.of(context).primaryText,
+                fontSize: _width / (_maxWidth / 20),
+              ),
+        );
+      }
     }),
     actions: [
       Padding(
