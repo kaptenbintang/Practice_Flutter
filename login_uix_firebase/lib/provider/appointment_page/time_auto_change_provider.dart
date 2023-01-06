ab732234a4d642eb1dc7de2287e60d2f89b9ea72
FaberKing <zoys.toys@gmail.com>
2023-01-06 17:49:49 +0800

    make new provider for change if value is exist for date and service

---
+++ login_uix_firebase/lib/provider/appointment_page/time_auto_change_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final servicesChangeProvider = StateProvider<bool?>((ref) => false);
+
+final dateChangeProvider = StateProvider<bool?>((ref) => false);
