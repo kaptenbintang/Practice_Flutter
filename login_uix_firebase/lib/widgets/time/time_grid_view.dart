5b5933c6fdff94d782113e6b87c2a14770dc9ac1
FaberKing <zoys.toys@gmail.com>
2023-01-06 17:53:15 +0800

    add grid view builder for Time and its per item view

--- login_uix_firebase/lib/widgets/time/time_grid_view.dart
+++ login_uix_firebase/lib/widgets/time/time_grid_view.dart
@@ -1,24 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:login_uix_firebase/provider/appointment_page/time_loop_provider.dart';
+import 'package:login_uix_firebase/widgets/animations/small_error_animation_view.dart';
+import 'package:login_uix_firebase/widgets/animations/small_loading_animation_view.dart';
 import 'package:login_uix_firebase/widgets/time/time_thumbnail_view.dart';
 
 class TimesGridView extends StatelessWidget {
   final String serviceTime;
-  final Map schedule;
-  const TimesGridView(
-      {super.key, required this.serviceTime, required this.schedule});
+  final String selectedTime;
+  final Map<dynamic, dynamic> schedule;
+  const TimesGridView({
+    super.key,
+    required this.serviceTime,
+    required this.schedule,
+    required this.selectedTime,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final intValue = int.parse(serviceTime.replaceAll(RegExp('[^0-9]'), ''));
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        mainAxisSpacing: 8.0,
-        crossAxisSpacing: 10.0,
-        childAspectRatio: 1,
-      ),
-      itemBuilder: (context, index) {
-        // return TimesThumbnailView(practioner: practioner, onTapped: onTapped);
+    return Consumer(
+      builder: (context, ref, child) {
+        final timeLoop = ref.watch(timeLoopProvider(Parameters(
+            schedules: schedule,
+            serviceTipe: serviceTime,
+            dateSelected: selectedTime)));
+        return timeLoop.when(
+          data: (data) {
+            return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                mainAxisSpacing: 500,
+                crossAxisSpacing: 100,
+                childAspectRatio: 1,
+              ),
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                final oneItem = data.elementAt(index);
+                return TimesThumbnailView(
+                  serviceTime: oneItem,
+                  onTapped: () {},
+                );
+                // return TimesThumbnailView(practioner: practioner, onTapped: onTapped);
+              },
+            );
+          },
+          error: (error, stackTrace) {
+            print(error);
+            return SmallErrorAnimationView();
+          },
+          loading: () {
+            return SmallLoadingAnimationView();
+          },
+        );
+        // return GridView.builder(
+        //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        //     crossAxisCount: 4,
+        //     mainAxisSpacing: 2.0,
+        //     crossAxisSpacing: 5.0,
+        //     childAspectRatio: 1,
+        //   ),
+        //   itemCount: timeLoop.length,
+        //   itemBuilder: (context, index) {
+        //     final oneItem = timeLoop.elementAt(index);
+        //     return TimesThumbnailView(
+        //       serviceTime: oneItem,
+        //       onTapped: () {},
+        //     );
+        //     // return TimesThumbnailView(practioner: practioner, onTapped: onTapped);
+        //   },
+        // );
       },
     );
   }
