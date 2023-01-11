eb0ba8a6323a93cb6f23fd51f9f7df79e6ed5f4a
Zuffy <you@example.comhanafirohani.2902@gmail.com>
2023-01-11 12:37:15 +0800

    revamp forget password page

--- login_uix_firebase/lib/pages/forgot_pw_page.dart
+++ login_uix_firebase/lib/pages/forgot_pw_page.dart
@@ -3,7 +3,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:login_uix_firebase/flutter_flow/flutter_flow.dart';
 
+import '../helper/responsive.dart';
 import '../main.dart';
 import 'check_email_page.dart';
 
@@ -53,96 +55,98 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blueAccent[200],
-          elevation: 0,
-        ),
+        appBar: (!ResponsiveWidget.isLargeScreen(context))
+            ? AppBar(
+                backgroundColor: FlutterFlowTheme.of(context).secondaryColor,
+                elevation: 0,
+              )
+            : null,
         body: Container(
+          width: double.maxFinite,
           color: Colors.white,
           child: SingleChildScrollView(
             child: Padding(
-              padding: const EdgeInsets.all(20.0),
+              padding: EdgeInsets.all(20.0),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: ResponsiveWidget.isLargeScreen(context)
+                    ? CrossAxisAlignment.center
+                    : CrossAxisAlignment.start,
                 children: [
                   SizedBox(
                     height: 10,
                   ),
-                  Text(
-                    'Forgot Password?',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold),
-                  ),
+                  Text('Forgot Password?',
+                      style: FlutterFlowTheme.of(context).title1),
                   SizedBox(
                     height: 10,
                   ),
                   Text(
                     'Enter the email associated with your account and we\'ll send an email with instructions to reset your password',
-                    style: TextStyle(color: Color(0xFF8a929f), fontSize: 15),
+                    style: FlutterFlowTheme.of(context).bodyText2,
                   ),
                   SizedBox(
                     height: 12,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: Text(
-                          'Email',
-                          style:
-                              TextStyle(fontSize: 15, color: Colors.grey[600]),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      'Email',
+                      style: FlutterFlowTheme.of(context).subtitle1,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 500,
+                    child: TextFormField(
+                      controller: _emailController,
+                      // maxLines: 1,
+                      // maxLength: 6,
+                      decoration: InputDecoration(
+                        hintText: "Enter your email address",
+                        hintStyle: FlutterFlowTheme.of(context).subtitle2,
+                        counterText: '',
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: BorderSide(color: Colors.grey),
                         ),
-                      ),
-                      TextFormField(
-                        controller: _emailController,
-                        // maxLines: 1,
-                        // maxLength: 6,
-                        decoration: InputDecoration(
-                          hintText: "Enter your email address",
-                          hintStyle: TextStyle(color: Color(0xFF6f6f6f)),
-                          counterText: '',
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15.0),
-                            borderSide: BorderSide(color: Colors.grey),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: BorderSide(
+                            color: Color(0xFFf3f5f6),
+                            width: 2.0,
                           ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15.0),
-                            borderSide: BorderSide(
-                              color: Color(0xFFf3f5f6),
-                              width: 2.0,
-                            ),
-                          ),
-                          fillColor: Colors.white,
-                          filled: true,
                         ),
+                        fillColor: Colors.white,
+                        filled: true,
                       ),
-                    ],
+                      textAlign: ResponsiveWidget.isLargeScreen(context)
+                          ? TextAlign.center
+                          : TextAlign.start,
+                    ),
                   ),
                   SizedBox(
                     height: 30,
                   ),
-                  Container(
+                  Center(
                     child: MaterialButton(
                       onPressed: passwordReset,
                       textColor: Colors.white,
-                      padding: const EdgeInsets.all(0.0),
+                      padding: EdgeInsets.all(0.0),
                       child: Container(
-                        height: 55,
-                        width: 400,
-                        decoration: new BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            color: Colors.blue),
-                        padding: const EdgeInsets.all(10.0),
+                        height: 30,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: FlutterFlowTheme.of(context).secondaryColor),
+                        padding: EdgeInsets.all(4),
                         child: Center(
                           child: Text(
                             "Send a Instructions!",
                             textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 17),
+                            style:
+                                FlutterFlowTheme.of(context).bodyText1.override(
+                                      fontFamily: 'Poppins',
+                                      color: Colors.white,
+                                    ),
                           ),
                         ),
                       ),
